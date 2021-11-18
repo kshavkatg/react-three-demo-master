e094127b80f3c73aac5820c318d3ea317fe6ddda
Shavkat <kshavkatg@gmail.com>
2021-11-18 16:27:11 +0600

    Updates

--- precache-manifest.115b4e2b7eb656e7881c7207809a1db7.js
+++ precache-manifest.115b4e2b7eb656e7881c7207809a1db7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65a2432f50810df76bffb18922917fd7",
+    "revision": "9558c329936e47840d1b43df5582e484",
     "url": "/react-three-demo-master/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-three-demo-master/static/js/2.7f84b728.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d86aeacd48d268fadcc4",
-    "url": "/react-three-demo-master/static/js/main.5ff7d8dc.chunk.js"
+    "revision": "a0fc7109f8c7402adad8",
+    "url": "/react-three-demo-master/static/js/main.b0b52559.chunk.js"
   },
   {
     "revision": "4a2eb19f066d33a520a5",
