cd3f21cae705890011324c4144b0b11d172bf2da
Shavkat <kshavkatg@gmail.com>
2021-11-18 18:28:47 +0600

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import * as THREE from 'three';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
-import { XREstimatedLight } from 'three/examples/webxr/XREstimatedLight.js';
+import { XREstimatedLight } from '../lib/XREstimatedLight';
 
 function Container() {
     useEffect(() => {
