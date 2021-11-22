99adc54436245f6bb4a6528c8c8fe96d3e775170
Shavkat <kshavkatg@gmail.com>
2021-11-22 11:35:58 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -138,9 +138,6 @@ function Container() {
             renderer.render( scene, camera );
         }
 
-        const ARButton = document.getElementById('ARButton')
-        ARButton.backgroundImage = 'url("./images/start_button.png")'
-
     }, [])
 
     return (
