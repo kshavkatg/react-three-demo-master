9b6f4c7a47b20e578647b346688af2eb737d2507
Shavkat <kshavkatg@gmail.com>
2021-11-22 11:47:54 +0600

    fix button 1

--- src/libs/ARButton.js
+++ src/libs/ARButton.js
@@ -135,18 +135,21 @@ class ARButton {
 
 		function stylizeElement( element ) {
 			element.style.position = 'absolute';
-			element.style.bottom = '20px';
-			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
-			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
-			element.style.color = '#fff';
-			element.style.font = 'normal 13px sans-serif';
-			element.style.textAlign = 'center';
-			element.style.opacity = '0.5';
-			element.style.outline = 'none';
+			element.style.bottom = '18%';
+			// element.style.padding = '12px 6px';
+			// element.style.border = '1px solid #fff';
+			// element.style.borderRadius = '4px';
+			// element.style.background = 'rgba(0,0,0,0.1)';
+			// element.style.color = '#fff';
+			// element.style.font = 'normal 13px sans-serif';
+			// element.style.textAlign = 'center';
+			// element.style.opacity = '0.5';
+			// element.style.outline = 'none';
+			element.style.width = '75%'
+			element.style.height = '90px'
 			element.style.zIndex = '999';
         	element.style.backgroundImage = 'url("./images/start_button.png")'
+			element.style.backgroundSize = 'cover'
 		}
 
 		if ( 'xr' in navigator ) {
