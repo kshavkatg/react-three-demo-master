8e9e69578189bc43928ba65d8c8cb2c0fcc4a6e0
Shavkat <kshavkatg@gmail.com>
2021-11-30 19:42:58 +0600

    Updates

--- precache-manifest.801c12fd41ca5ca66021dd9dd578807b.js
+++ precache-manifest.801c12fd41ca5ca66021dd9dd578807b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef5871e0deb5f97e1bc4a16e322643eb",
+    "revision": "a609d8b785fc23ce0478ba8a25bef7d4",
     "url": "/react-three-demo-master/index.html"
   },
   {
-    "revision": "f853b62f025110b247f7",
+    "revision": "9cf7cb536ff5dc6def42",
     "url": "/react-three-demo-master/static/css/main.eb19292f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-three-demo-master/static/js/2.8adbdd3e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f853b62f025110b247f7",
-    "url": "/react-three-demo-master/static/js/main.d0892013.chunk.js"
+    "revision": "9cf7cb536ff5dc6def42",
+    "url": "/react-three-demo-master/static/js/main.7c38d6a7.chunk.js"
   },
   {
     "revision": "a00fc578919129684d13",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-three-demo-master/precache-manifest.437e7c799b95eac3c349702d87699667.js"
+  "/react-three-demo-master/precache-manifest.801c12fd41ca5ca66021dd9dd578807b.js"
 );
 
 self.addEventListener('message', (event) => {
