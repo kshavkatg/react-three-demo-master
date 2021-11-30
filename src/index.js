12a475c16941915ce5d5fac7bf0421646edc74b3
Shavkat <kshavkatg@gmail.com>
2021-11-30 18:47:31 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -87,8 +87,8 @@ function Container() {
 
             document.body.addEventListener('click', onTouch)
             document.body.addEventListener('touchmove', (e)=> {
-                console.log('x', e.touches.clientX)
-                console.log('x', e.touches.clientX)
+                console.log('x', e.touches[0].clientX)
+                console.log('y', e.touches[0].clientX)
             })
 
 
