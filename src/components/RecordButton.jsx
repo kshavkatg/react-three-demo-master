e9707a0e97ee383224ae3e454cb358d19bc5dc11
Shavkat <kshavkatg@gmail.com>
2021-12-01 17:20:58 +0600

    remove displaly: none, and add fade-container instead

--- src/components/RecordButton.jsx
+++ src/components/RecordButton.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function RecordButton() {
     return (
         <>
-            <div id="recorder" className="recorder-container">
+            <div id="recorder" className="recorder-container fade-container">
                 <svg viewBox="0 0 38 38" className="progress-container">
                   <circle className="progress-track" r="16" cx="19" cy="19"></circle>
                   <circle id="progressBar" className="progress-bar" r="16" cx="19" cy="19"></circle>
