5903111d1cb5cc94bda3544e80d9c8bb788c4ea0
Shavkat <kshavkatg@gmail.com>
2021-12-01 17:08:08 +0600

    fix classname

--- src/components/RecordButton.jsx
+++ src/components/RecordButton.jsx
@@ -3,19 +3,19 @@ import React from "react"
 export default function RecordButton() {
     return (
         <>
-            <div id="recorder" classList="recorder-container fade-container">
-                <svg viewBox="0 0 38 38" classList="progress-container">
-                  <circle classList="progress-track" r="16" cx="19" cy="19"></circle>
-                  <circle id="progressBar" classList="progress-bar" r="16" cx="19" cy="19"></circle>
-                  <circle classList="loading-circle" r="16" cx="19" cy="19"></circle>
+            <div id="recorder" className="recorder-container">
+                <svg viewBox="0 0 38 38" className="progress-container">
+                  <circle className="progress-track" r="16" cx="19" cy="19"></circle>
+                  <circle id="progressBar" className="progress-bar" r="16" cx="19" cy="19"></circle>
+                  <circle className="loading-circle" r="16" cx="19" cy="19"></circle>
                 </svg>
 
-                <button id="recorder-button" classList="style-reset">
+                <button id="recorder-button" className="style-reset">
                   Record
                 </button>
               </div>
 
-            <div id="flashElement" classList="flash-element"></div>
+            <div id="flashElement" className="flash-element"></div>
         </>
     )
 }
