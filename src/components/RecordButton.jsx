5408b20465b1cad4065b50c07c5c68b7b05a31ee
Shavkat <kshavkatg@gmail.com>
2021-12-01 16:59:21 +0600

    import css to custom

--- src/components/RecordButton.jsx
+++ src/components/RecordButton.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function RecordButton() {
     return (
         <>
-            <div id="recorder" classList="recorder-container fade-container ">
+            <div id="recorder" classList="recorder-container fade-container">
                 <svg viewBox="0 0 38 38" classList="progress-container">
                   <circle classList="progress-track" r="16" cx="19" cy="19"></circle>
                   <circle id="progressBar" classList="progress-bar" r="16" cx="19" cy="19"></circle>
