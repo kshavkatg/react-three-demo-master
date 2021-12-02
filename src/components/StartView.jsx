e7588fadd99d8f8c73e614b7311bc7d6d7884b92
Shavkat <kshavkatg@gmail.com>
2021-12-02 15:44:28 +0600

    clear all recorder tests

--- src/components/StartView.jsx
+++ src/components/StartView.jsx
@@ -1,35 +1,6 @@
 import React, { useEffect } from "react"
-import * as RecordRTC from 'recordrtc';
 
 export default function StartView({onStartClick}) {
-  
-  useEffect(() => {
-    const recordButton = document.querySelector('.start_view_heart')
-    const htmlElement = document.createElement('canvas')
-  
-    recordButton.addEventListener('click', () => {
-      console.log('record start')
-      navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-      }).then(async function(stream) {
-        let recorder = new RecordRTC.CanvasRecorder(htmlElement, { disableLogs: true, useWhammyRecorder: true });
-        recorder.startRecording();
-        console.log('recorder', recorder)
-    
-        const sleep = m => new Promise(r => setTimeout(r, m));
-        await sleep(3000);
-    
-        recorder.stopRecording(function() {
-            let blob = recorder.getBlob();
-            RecordRTC.invokeSaveAsDialog(blob);
-            
-        });
-      });
-    });
-    document.body.appendChild(htmlElement)
-    
-  }, [])
 
   return (
     <>
