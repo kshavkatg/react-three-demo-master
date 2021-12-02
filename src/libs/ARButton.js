9704d95763e52d10715b9d7684419bc6ae8543de
Shavkat <kshavkatg@gmail.com>
2021-12-02 16:48:29 +0600

    fix

--- src/libs/ARButton.js
+++ src/libs/ARButton.js
@@ -218,7 +218,7 @@ class ARButton {
 			message.style.textDecoration = 'none';
 
 			stylizeElement( message );
-			message.style.display = 'none'
+			message.style.backgroundImage = 'none'
 			message.style.color = 'white'
 			message.style.fontSize = '20px'
 
