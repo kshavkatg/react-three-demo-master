0c3d0a19902f1d93c40a381034a828ded5fb3f17
Shavkat <kshavkatg@gmail.com>
2021-12-02 14:42:44 +0600

    test use recortRtc

--- src/components/RecordButton.jsx
+++ src/components/RecordButton.jsx
@@ -3,83 +3,94 @@ import React, { useEffect } from "react"
 export default function RecordButton() {
 
   useEffect(() => {
-    const video = document.getElementById( 'greenscreenvideo' );
-		const startVideoButton = document.querySelector('.start_video')
-		const recordButton = document.querySelector('#recorder')
-    let stream, recorder, mixedStream, audio, recordedVideo;
-    let chunks = [];
+  //   const video = document.getElementById( 'greenscreenvideo' );
+	// 	const startVideoButton = document.querySelector('.start_video')
+	// 	const recordButton = document.querySelector('#recorder')
+  //   let stream, recorder, mixedStream, audio, recordedVideo;
+  //   let chunks = [];
 
-  async function setupStream () {
-    try {
+  // async function setupStream () {
+  //   try {
   
-      audio = await navigator.mediaDevices.getUserMedia({
-        audio: {
-          echoCancellation: true,
-          noiseSuppression: true,
-          sampleRate: 44100,
-        },
-        video: { width: 1280, height: 720 }
-      });
+  //     audio = await navigator.mediaDevices.getUserMedia({
+  //       audio: {
+  //         echoCancellation: true,
+  //         noiseSuppression: true,
+  //         sampleRate: 44100,
+  //       },
+  //       video: { width: 1280, height: 720 }
+  //     });
   
-    } catch (err) {
-      console.error(err)
-    }
-  }
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  // }
 
-  async function startRecording () {
-    await setupStream();
+  // async function startRecording () {
+  //   await setupStream();
   
-    if (audio) {
-      mixedStream = new MediaStream([ ...audio.getTracks()]);
-      recorder = new MediaRecorder(mixedStream);
-      recorder.ondataavailable = handleDataAvailable;
-      recorder.onstop = handleStop;
-      recorder.start(1000);
+  //   if (audio) {
+  //     mixedStream = new MediaStream([ ...audio.getTracks()]);
+  //     recorder = new MediaRecorder(mixedStream);
+  //     recorder.ondataavailable = handleDataAvailable;
+  //     recorder.onstop = handleStop;
+  //     recorder.start(1000);
     
-      // startButton.disabled = true;
-      // stopButton.disabled = false;
+  //     // startButton.disabled = true;
+  //     // stopButton.disabled = false;
     
-      console.log('Recording started');
-    } else {
-      console.warn('No stream available.');
-    }
-  }
+  //     console.log('Recording started');
+  //   } else {
+  //     console.warn('No stream available.');
+  //   }
+  // }
 
-  function stopRecording () {
-    recorder.stop();
+  // function stopRecording () {
+  //   recorder.stop();
   
-    // startButton.disabled = false;
-    // stopButton.disabled = true;
-  }
+  //   // startButton.disabled = false;
+  //   // stopButton.disabled = true;
+  // }
   
-  function handleDataAvailable (e) {
-    chunks.push(e.data);
-  }
+  // function handleDataAvailable (e) {
+  //   chunks.push(e.data);
+  // }
 
-  function handleStop (e) {
-    const blob = new Blob(chunks, { 'type' : 'video/mp4' });
-    chunks = [];
+  // function handleStop (e) {
+  //   const blob = new Blob(chunks, { 'type' : 'video/mp4' });
+  //   chunks = [];
   
-    // downloadButton.href = URL.createObjectURL(blob);
-    // downloadButton.download = 'video.mp4';
-    // downloadButton.disabled = false;
+  //   // downloadButton.href = URL.createObjectURL(blob);
+  //   // downloadButton.download = 'video.mp4';
+  //   // downloadButton.disabled = false;
   
-    recordedVideo.src = URL.createObjectURL(blob);
-    recordedVideo.load();
-    recordedVideo.onloadeddata = function() {
-      const rc = document.querySelector(".recorded-video-wrap");
-      rc.classList.remove("hidden");
-      rc.scrollIntoView({ behavior: "smooth", block: "start" });
+  //   recordedVideo.src = URL.createObjectURL(blob);
+  //   recordedVideo.load();
+  //   recordedVideo.onloadeddata = function() {
+  //     const rc = document.querySelector(".recorded-video-wrap");
+  //     rc.classList.remove("hidden");
+  //     rc.scrollIntoView({ behavior: "smooth", block: "start" });
   
-      recordedVideo.play();
-    }
+  //     recordedVideo.play();
+  //   }
   
-    audio.getTracks().forEach((track) => track.stop());
+  //   audio.getTracks().forEach((track) => track.stop());
   
-    console.log('Recording stopped');
-  }
-  recordButton.addEventListener('click', startRecording);
-
+  //   console.log('Recording stopped');
+  // }
+  // recordButton.addEventListener('click', startRecording);
+  let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+  let recorder = new RecordRTCPromisesHandler(stream, {
+      type: 'video'
+  });
+  recorder.startRecording();
+  
+  const sleep = m => new Promise(r => setTimeout(r, m));
+  await sleep(3000);
+  
+  await recorder.stopRecording();
+  let blob = await recorder.getBlob();
+  invokeSaveAsDialog(blob);
 
 }, [])
 
