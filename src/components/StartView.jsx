8688bd6a894027fa48534cfe3900f6b9677542b6
Shavkat <kshavkatg@gmail.com>
2021-12-02 15:10:11 +0600

    test start record before starting ar session, add record on heart click

--- src/components/StartView.jsx
+++ src/components/StartView.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react"
+import * as RecordRTC from 'recordrtc';
 
 export default function StartView({onStartClick}) {
   
+  useEffect(() => {
+    const recordButton = document.querySelector('.start_view_heart')
+
+  
+    recordButton.addEventListener('click', () => {
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      }).then(async function(stream) {
+        let recorder = RecordRTC(stream, {
+            type: 'video'
+        });
+        recorder.startRecording();
+    
+        const sleep = m => new Promise(r => setTimeout(r, m));
+        await sleep(3000);
+    
+        recorder.stopRecording(function() {
+            let blob = recorder.getBlob();
+            RecordRTC.invokeSaveAsDialog(blob);
+            
+        });
+      });
+    });
+    
+  }, [])
+
   return (
     <>
       <div className='start_view'>
